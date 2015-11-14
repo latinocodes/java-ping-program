e2544122b1ceb9cad82ebdfe5f5d3e85519b651b
Jose Lara <Jose@Joses-MBP.home>
2015-11-14 01:16:37 -0500

    here

--- src/PingProgram.java
+++ src/PingProgram.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class weekFourProgram {
+public class PingProgram {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
